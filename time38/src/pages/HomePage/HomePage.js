1ca3f8a82329d329a950747f98d13dbcd5db4fbe
ArgLD <argleydson@gmail.com>
2021-11-28 18:49:11 -0300

    edição da HomePage, mudança dos botões da Home e do Login

--- time38/src/pages/HomePage/HomePage.js
+++ time38/src/pages/HomePage/HomePage.js
@@ -2,33 +2,40 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import { goToLogin } from '../../routes/coordinator';
-import { HomePageContainer } from './styled';
+import {
+  ButtonContainer,
+  DescriptionContainer,
+  HomePageContainer,
+} from './styled';
 
 const HomePage = () => {
   const navigate = useNavigate();
   return (
     <HomePageContainer>
-      <h1>LOGO</h1>
-      <div>
+      <h1>Maiorise</h1>
+      <DescriptionContainer>
+        <p>
+          Somos a Maiorise , focados em conectar pessoas pertencentes a grupos
+          minoritários a oportunidades profissionais. Levamos esse nome pois
+          sabemos que minoritarias são as chances dadas a estas pessoas, por
+          isso, através da nossa plataforma criamos conexões entre estes
+          individuos e as empresas que buscam aumentar a diversidade e
+          consequentemente suas capacidades de crescimento. Conheça nossa
+          plataforma:
+        </p>
+      </DescriptionContainer>
+
+      <ButtonContainer>
         <Button
           fullWidth
           variant={'contained'}
           color={'primary'}
           onClick={() => goToLogin(navigate)}
         >
-          Usuário
+          Logar
         </Button>
-      </div>
-      <div>
-        <Button
-          fullWidth
-          variant={'contained'}
-          color={'primary'}
-          onClick={() => goToLogin(navigate)}
-        >
-          Empresa
-        </Button>
-      </div>
+       
+      </ButtonContainer>
     </HomePageContainer>
   );
 };
